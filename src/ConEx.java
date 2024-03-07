d0ca5bb620d1205b9504b79104d0d8eb557b4591
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-03-07 11:32:19 +0530

    connection url was wrong, now improved

--- src/ConEx.java
+++ src/ConEx.java
@@ -1,12 +1,12 @@
 import java.sql.*;
 
 public class ConEx {
-    public static void main() {
+    public static void main(String[] args) {
         try {
             // Register the Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
             // Create the connection
-            Connection con = DriverManager.getConnection("mysql.jdbc://localhost:3306/yt", "root", "somaiya");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/youtube", "root", "somaiya");
             if (con.isClosed()) {
                 System.out.println("not connected");
             } else {
