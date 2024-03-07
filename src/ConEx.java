d5d1f324cbed1573640a98d8c9f7bb2874381456
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-03-07 11:22:46 +0530

    Directory improved

--- src/ConEx.java
+++ src/ConEx.java
@@ -1,15 +1,19 @@
+import java.sql.*;
+
 public class ConEx {
     public static void main() {
-        try{
-            //Register the Driver
+        try {
+            // Register the Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-            //Create the connection
+            // Create the connection
             Connection con = DriverManager.getConnection("mysql.jdbc://localhost:3306/yt", "root", "somaiya");
-            if(con.isClosed()) {
+            if (con.isClosed()) {
                 System.out.println("not connected");
             } else {
                 System.out.println("Connected");
             }
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 }
